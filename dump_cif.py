477798afd2a37c470605272127972ccf5b010d54
Kishalay <kishalaydas@iisc.ac.in>
2021-01-03 11:18:09 +0530

    new pythonfile

--- dump_cif.py
+++ dump_cif.py
@@ -1,9 +1,13 @@
 import pandas as pd
 from qmpy import *
 
-data=pd.read_csv("experimental_prop.csv").values[:,0]
-composition=data[0]
+# data=pd.read_csv("experimental_prop.csv").values[:,0]
+# for i in range(len(data)):
+#     composition=data[0]
+#     entry=Element.get(composition)
+# print(entry)
+# print("Hello")
 
-entry=Element.get(composition)
-print(entry)
-print("Hello")
+
+s = io.read('qmpy/io/files/POSCAR_BCC')
+print(s)
