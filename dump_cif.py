7ec395ec40b1d89e98c7a20ba8e417a76f7d5d8d
Kishalay <kishalaydas@iisc.ac.in>
2021-01-03 16:20:14 +0530

    new pythonfile

--- dump_cif.py
+++ dump_cif.py
@@ -4,7 +4,7 @@ from qmpy import *
 data=pd.read_csv("exp_data/experimental_prop.csv").values[:,0]
 print("Hello")
 for i in range(len(data)):
-    composition=data[0]
+    composition=data[i]
     entry=Element.objects.get(symbol=composition)
     structure =Structure(entry)
     io.cif.write(structure, "exp_data/"+str(i)+".cif")
